37edbf2698a50b21c1d5ea6f2c3a20cf82840d4b
vipulshete <vipulshete724@gmail.com>
2022-07-27 17:55:02 +0530

    string max

--- SortExample.java
+++ SortExample.java
@@ -8,28 +8,26 @@ public class SortExample {
 
 	public static void main(String[] args) {
 		
-		sortIntegerList();
+		sortStringList();
 	}
 	
-	private static void sortIntegerList() {
-		List<Double> integerList = new ArrayList<>();
+	private static void sortStringList() {
+		List<String> stringList = new ArrayList<>();
+		stringList.add("vipul");
+		stringList.add("tejal");
+		stringList.add("anusha");
+		stringList.add("sameeksha");
+		stringList.add("surya");
 		
-		integerList.add(35.31);
-		integerList.add(67.22);
-		integerList.add(12.25);
-		integerList.add(17.25);
-		integerList.add(9.14);
-		integerList.add(88.41);
+		System.out.println("Before sorting " + stringList);
 		
-		System.out.println("Before sorting " + integerList);
-		
-		Collections.sort(integerList);
-		Collections.reverse(integerList);
+		Collections.sort(stringList);
+		Collections.reverse(stringList);
 		//System.out.println("After reversing " + integerList);
 		
-		System.out.println("First maximum " + integerList.get(0));
-		System.out.println("Second maximum " + integerList.get(1));
-		System.out.println("Third maximum " + integerList.get(2));
+		System.out.println("First maximum " + stringList.get(0));
+		System.out.println("Second maximum " + stringList.get(1));
+		System.out.println("Third maximum " + stringList.get(2));
 	}
 	
 }
